92bcd9989f18955ff85906360941df3add55960c
wilson-bg <wbeltran@millionandup.com>
2022-09-10 18:11:06 -0500

    Pruebas de carga

--- 4. Capa de datos/postgresMonitoring/commands.cmd
+++ 4. Capa de datos/postgresMonitoring/commands.cmd
@@ -7,3 +7,5 @@ python manage.py runserver 0.0.0.0:8000
 .env\Scripts\deactivate
 
 
+wget https://github.com/Uniandes-MISO/IoT/raw/main/4.%20Capa%20de%20datos/postgresMonitoring.zip -O server_f.zip
+unzip server_f.zip
